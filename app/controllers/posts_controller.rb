3c072a7b9886077a004d03da2bb578385a168089
Muhammad_shahzaib <shahzaibmss36@gmail.com>
2024-05-17 10:30:19 -0400

    commnets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,7 @@ class PostsController < ApplicationController
   end
   def contact
   end
+  def show
+    @post = Post.find(params[:id])
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get "post", to: 'posts#post' 
   get "marketing", to: 'posts#marketing' 
   get "contact", to: 'posts#contact' 
+  get '/posts/:id', to: 'posts#show', as: 'show_post'
   root "posts#home"
 end
